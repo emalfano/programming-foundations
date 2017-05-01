4dcbfce3665037d0d3330b9921f19984aa2528aa
Elizabeth Alfano <emalfano1@gmail.com>
2017-05-01 22:17:19 +0000

    lesson 3 complete

--- lesson_3/hard_1.rb
+++ lesson_3/hard_1.rb
@@ -31,6 +31,7 @@ puts greetings
 # which returns a new String object instead of modifying the original object.
 
 # Question 3
+
 # In other exercises we have looked at how the scope of variables affects the 
 # modification of one "layer" when they are passed to another.
 
@@ -53,9 +54,11 @@ three = "three"
 
 mess_with_vars(one, two, three)
 
+puts 'ema: this should print "one" "two" and "three"'
 puts "one is: #{one}"
 puts "two is: #{two}"
 puts "three is: #{three}"
+
 # B)
 
 def mess_with_vars(one, two, three)
@@ -70,6 +73,7 @@ three = "three"
 
 mess_with_vars(one, two, three)
 
+puts 'ema: this should print "one" "two" and "three"'
 puts "one is: #{one}"
 puts "two is: #{two}"
 puts "three is: #{three}"
@@ -87,11 +91,38 @@ three = "three"
 
 mess_with_vars(one, two, three)
 
+puts 'ema: This should print "two" "three" and "one"'
 puts "one is: #{one}"
 puts "two is: #{two}"
 puts "three is: #{three}"
 
 
 # Question 4
+# Ben was tasked to write a simple ruby method to determine if an input string 
+# is an IP address representing dot-separated numbers. e.g. "10.4.5.11". He is
+# not familiar with regular expressions. Alyssa supplied Ben with a method
+# called is_an_ip_number? that determines if a string is a valid ip address
+# number and asked Ben to use it.
 
+def dot_separated_ip_address?(input_string)
+  dot_separated_words = input_string.split(".")
+  if dot_separated_words.size != 4
+    return false
+  end
+  # ls soln: return false unless dot_separated_words.size == 4
+  while dot_separated_words.size > 0 do
+    word = dot_separated_words.pop
+    break unless is_an_ip_number?(word)
+    return false
+    # ls soln: return false unless is_an_ip_number?(word)
+  end
+  return true # change to just true b/c ruby returns the result of the last
+  # evaluated statement
+end
+# Alyssa reviewed Ben's code and says "It's a good start, but you missed a few 
+# things. You're not returning a false condition, and you're not handling the 
+# case that there are more or fewer than 4 components to the IP address
+# (e.g. "4.5.5" or "1.2.3.4.5" should be invalid)."
+
+# Help Ben fix his code.
 
