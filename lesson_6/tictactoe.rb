73ff38d598d01abde0a7c0d1fc50ee9161781602
Elizabeth Alfano <emalfano1@gmail.com>
2017-06-06 22:40:14 +0000

    Tic tac toe bonus features - player scores

--- lesson_6/tictactoe.rb
+++ lesson_6/tictactoe.rb
@@ -1,12 +1,45 @@
-# require 'pry'
+require 'pry'
 # tic tac toe game
 
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
+                [[1, 5, 9], [3, 5, 7]]              # diagonals
 
 def prompt(msg)
   puts "=> #{msg}"
+end
+
+# Method to display empty squares with an "or" before last square
+# def joinor(empty_squares_array, separator=',', operand='or')
+#   formatted_array = ''
+#   empty_squares_array.each_with_index do |square, index|
+#     if empty_squares_array.size == 1
+#       formatted_array = square
+#     elsif empty_squares_array.size == 2 && index == 0
+#     # this condition won't ever happen?
+#       formatted_array << square.to_s + ' '
+#     elsif index == empty_squares_array.size - 1
+#       formatted_array << operand + ' ' + square.to_s
+#     else
+#       formatted_array << square.to_s + separator + ' '
+#     end
+#   end
+#   formatted_array
+# end
+
+# LS solution suggests use case statement like this:
+def joinor(arr, delimiter=', ', word='or')
+  case arr.size
+  when 0 then ''
+  when 1 then arr.first
+  when 2 then arr.join(" #{word} ")
+  else
+    arr[-1] = "#{word} #{arr.last}"
+    arr.join(delimiter)
+  end
 end
 
 # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
@@ -42,7 +75,7 @@ end
 def player_places_piece!(brd)
   square = ''
   loop do
-    prompt "Choose a square (#{empty_squares(brd).join(',')}):"
+    prompt "Choose a square (#{joinor(empty_squares(brd))}):"
     square = gets.chomp.to_i
     # binding.pry
     break if empty_squares(brd).include?(square)
@@ -67,30 +100,38 @@ def someone_won?(brd)
 end
 
 def detect_winner(brd)
-  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
-                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
-                  [[1, 5, 9], [3, 5, 7]]              # diagonals
-
-  winning_lines.each do |line|
-    if brd[line[0]] == PLAYER_MARKER &&
-       brd[line[1]] == PLAYER_MARKER &&
-       brd[line[2]] == PLAYER_MARKER
+  WINNING_LINES.each do |line|
+    # if brd[line[0]] == PLAYER_MARKER &&
+    #   brd[line[1]] == PLAYER_MARKER &&
+    #   brd[line[2]] == PLAYER_MARKER
+    #   return 'Player'
+    # elsif brd[line[0]] == COMPUTER_MARKER &&
+    #       brd[line[1]] == COMPUTER_MARKER &&
+    #       brd[line[2]] == COMPUTER_MARKER
+    #   return 'Computer'
+    # end
+    if brd.values_at(*line).count(PLAYER_MARKER) == 3
       return 'Player'
-    elsif brd[line[0]] == COMPUTER_MARKER &&
-          brd[line[1]] == COMPUTER_MARKER &&
-          brd[line[2]] == COMPUTER_MARKER
+    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
       return 'Computer'
     end
   end
   nil
 end
 
+player_wins = 0
+computer_wins = 0
+
 loop do
+  # win_counts = {'Player'=>0, 'Computer'=>0}
+  
   board = initialize_board
 
   # ask the user to mark a square
 
   loop do
+   
+
     display_board(board)
 
     player_places_piece!(board)
@@ -101,13 +142,23 @@ loop do
   end
 
   display_board(board)
-
+  
   if someone_won?(board)
     prompt "#{detect_winner(board)} won!"
+    if detect_winner(board) == 'Player'
+      player_wins += 1
+    elsif detect_winner(board) == 'Computer'
+      computer_wins += 1
+    end
+    prompt "Player: #{player_wins} to computer: #{computer_wins}"
   else
     prompt "It's a tie!"
   end
-
+  if player_wins == 5 || computer_wins == 5
+    prompt "#{detect_winner(board)} has won 5 games! Game Over!"
+    break
+  end
+  
   prompt "Do you want to play again? (y or n)"
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
